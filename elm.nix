b621b22ed65097eace8377708eb650ee781359e5
Dwayne Crooks <me@dwaynecrooks.com>
2025-02-20 16:42:24 -0400

    Add the tools I often use in my Elm projects

--- elm.nix
+++ elm.nix
@@ -23,11 +23,15 @@ stdenv.mkDerivation {
   # https://gist.github.com/CMCDragonkai/9b65cbb1989913555c203f4fa9c23374
   # https://github.com/NixOS/nixpkgs/blob/234bb31f611f43f8b744b305ab82035de637aaca/pkgs/build-support/setup-hooks/make-wrapper.sh
   #
-  postFixup = ''
+  postFixup = with elmPackages; ''
     wrapProgram $out/bin/elm \
       --set PATH ${lib.makeBinPath [
-        elmPackages.elm
-        elmPackages.elm-test
+        elm
+        elm-doc-preview
+        elm-format
+        elm-optimize-level-2
+        elm-review
+        elm-test
       ]}
   '';
 }
--- src/elm.sh
+++ src/elm.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env bash
 
-echo "Hello, world!"
-echo $(elm-test --version)
-echo $(elm --version)
+set -euo pipefail
+
+subcommand="${1:-help}"
+
+if [ "$#" -ne 0 ]; then
+  shift
+fi
+
+case "$subcommand" in
+  docs)
+    elm-doc-preview "$@"
+    ;;
+
+  format)
+    elm-format "$@"
+    ;;
+
+  optimize)
+    elm-optimize-level-2 "$@"
+    ;;
+
+  review)
+    elm-review "$@"
+    ;;
+
+  test)
+    elm-test "$@"
+    ;;
+
+  help)
+    elm
+    ;;
+
+  *)
+    elm "$subcommand" "$@"
+    ;;
+esac
